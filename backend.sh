3d6e7993c92c4decda43572be2752130894aea86
shree <shreesbadiger@gmail.com>
2023-11-03 14:48:16 +0530

    updated

--- backend.sh
+++ backend.sh
@@ -1,33 +1,33 @@
 echo -e "\e[31m Disabling Nodejs and installing Newer version \e[0m"
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
+dnf module disable nodejs -y &>>/tmp/expense.log
+dnf module enable nodejs:18 -y &>>/tmp/expense.log
+dnf install nodejs -y &>>/tmp/expense.log
 
 echo -e "\e[31m Enabling systemd service \e[0m"
-cp backend.service /etc/systemd/system/backend.service
+cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
 
 echo -e "\e[33m Creating user \e[Om"
-useradd expense
+useradd expense &>>/tmp/expense.log
 
 echo -e "\e[34m Creating a directory to run the app \e[0m"
-mkdir /app 
+mkdir /app &>>/tmp/expense.log
 
 echo -e "\e[35m Downloading and extracting dependencies for nodejs \e[0m"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip 
-cd /app 
-unzip /tmp/backend.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>/tmp/expense.log
+cd /app &>>/tmp/expense.log
+unzip /tmp/backend.zip &>>/tmp/expense.log
 
-cd /app 
-npm install 
+cd /app &>>/tmp/expense.log
+npm install &>>/tmp/expense.log
 
-dnf install mysql -y 
+dnf install mysql -y &>>/tmp/expense.log
 
-mysql -h 172.31.46.185 -uroot -pExpenseApp@1 < /app/schema/backend.sql 
+mysql -h 172.31.46.185 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/expense.log
 
-systemctl daemon-reload
+systemctl daemon-reload &>>/tmp/expense.log
 
-systemctl enable backend
+systemctl enable backend &>>/tmp/expense.log
 
-systemctl restart backend
+systemctl restart backend &>>/tmp/expense.log
 
 
