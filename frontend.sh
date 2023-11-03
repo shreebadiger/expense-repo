31c6f3626507b2f3395c5b4956dc54eca3a5eab6
shree <shreesbadiger@gmail.com>
2023-11-03 14:02:18 +0530

    updated

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
-echo -e "\e[31m Installing nginx \e[Om"
+echo -e "\e[31m Installing nginx \e[0m"
 dnf install nginx -y
 
-echo -e "\e[32m Replacing nginx default page \e[Om"
+echo -e "\e[32m Replacing nginx default page \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-echo -e "\e[33m Removing nginx default page\e[Om"
+echo -e "\e[33m Removing nginx default page\e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[34m Downloading frontend congiguration file \e[Om"
+echo -e "\e[34m Downloading frontend congiguration file \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip 
 
-echo -e "\e[35m Extracting the file \e[Om"
+echo -e "\e[35m Extracting the file \e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
-echo -e "\e[36m Starting nginx \e[Om"
+echo -e "\e[36m Starting nginx \e[0m"
 systemctl enable nginx 
 systemctl restart nginx 
