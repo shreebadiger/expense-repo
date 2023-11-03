d0dbf58380c174e4c7d5cd1a2e76b7e3c956f403
shree <shreesbadiger@gmail.com>
2023-11-03 22:02:21 +0530

    UPDATED

--- mysql.sh
+++ mysql.sh
@@ -3,21 +3,41 @@ log_file=&>>/tmp/expense.log
 
 echo -e "${color} Disabling default version of the mysql \e[0m"
 dnf module disable mysql -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo -e "\e[36m success \e[0m"
+else
+echo -e "\e[36m Failure \e[0m"
+fi
 
 echo -e "${color} Creating repo file \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo -e "\e[36m success \e[0m"
+else
+echo -e "\e[36m Failure \e[0m"
+fi
 
 echo -e "${color} Installing mysql-community-server\e[0m"
 dnf install mysql-community-server -y &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo -e "\e[36m success \e[0m"
+else
+echo -e "\e[36m Failure \e[0m"
+fi
 
 echo -e "${color} Starting mysql-community-server\e[0m"
 systemctl enable mysqld &>>log_file
 systemctl start mysqld &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo -e "\e[36m success \e[0m"
+else
+echo -e "\e[36m Failure \e[0m"
+fi
 
 echo -e "${color} Setting password to mysql-community-server\e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file
-echo $?
+if [ $? -eq 0 ]; then
+echo -e "\e[36m success \e[0m"
+else
+echo -e "\e[36m Failure \e[0m"
+fi
