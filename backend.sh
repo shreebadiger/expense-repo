8a94274d5a851331e731f3a907182c64f219c71c
shree <shreesbadiger@gmail.com>
2023-11-03 22:28:44 +0530

    updated

--- backend.sh
+++ backend.sh
@@ -26,9 +26,9 @@ else
 echo -e "\e[33m Failure \e[0m"
 fi
 
-echo -e "${color} Creating application user \e[0m"
 id userexpense &>>log_file
 if [ $? -ne 0 ]; then
+echo -e "${color} Creating application user \e[0m"
 useradd expense &>>log_file
 if [ $? -eq 0 ]; then
 echo -e "\e[33m success \e[0m"
