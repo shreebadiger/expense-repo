d3275655edfe6279ce1674d7737f711af5c03cbb
shree <shreesbadiger@gmail.com>
2023-11-03 14:00:58 +0530

    UPDATED

--- mysql.sh
+++ mysql.sh
@@ -1,16 +1,16 @@
 
-echo -e "\e[31m Disabling default version of the mysql \e[Om"
+echo -e "\e[31m Disabling default version of the mysql \e[0m"
 dnf module disable mysql -y 
 
-echo -e "\e[31m Creating repo file \e[Om"
+echo -e "\e[31m Creating repo file \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
-echo -e "\e[31m Installing mysql-community-server\e[Om"
+echo -e "\e[31m Installing mysql-community-server\e[0m"
 dnf install mysql-community-server -y
 
-echo -e "\e[31m Starting mysql-community-server\e[Om"
+echo -e "\e[31m Starting mysql-community-server\e[0m"
 systemctl enable mysql
 systemctl start mysql
 
-echo -e "\e[31m Setting password to mysql-community-server\e[Om"
+echo -e "\e[31m Setting password to mysql-community-server\e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1
