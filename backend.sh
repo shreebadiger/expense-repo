3f981f77d6a965cc8e4c1f64a7b9dc8c84185b04
shree <shreesbadiger@gmail.com>
2023-11-03 15:06:54 +0530

    updated

--- backend.sh
+++ backend.sh
@@ -1,18 +1,18 @@
-echo -e "\e[31m Disabling Nodejs and installing Newer version \e[0m"
+echo -e "\e[32m Disabling Nodejs and installing Newer version \e[0m"
 dnf module disable nodejs -y &>>/tmp/expense.log
 dnf module enable nodejs:18 -y &>>/tmp/expense.log
 dnf install nodejs -y &>>/tmp/expense.log
 
-echo -e "\e[31m Enabling systemd service \e[0m"
+echo -e "\e[32m Enabling systemd service \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
 
-echo -e "\e[33m Creating user \e[Om"
+echo -e "\e[32m Creating user \e[Om"
 useradd expense &>>/tmp/expense.log
 
-echo -e "\e[34m Creating a directory to run the app \e[0m"
+echo -e "\e[32m Creating a directory to run the app \e[0m"
 mkdir /app &>>/tmp/expense.log
 
-echo -e "\e[35m Downloading and extracting dependencies for nodejs \e[0m"
+echo -e "\e[32m Downloading and extracting dependencies for nodejs \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>/tmp/expense.log
 cd /app &>>/tmp/expense.log
 unzip /tmp/backend.zip &>>/tmp/expense.log
