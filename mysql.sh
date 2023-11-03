f2817100aed8213e61c1ef0f629c2878051f0f37
shree <shreesbadiger@gmail.com>
2023-11-03 14:48:49 +0530

    updated

--- mysql.sh
+++ mysql.sh
@@ -1,16 +1,16 @@
 
 echo -e "\e[31m Disabling default version of the mysql \e[0m"
-dnf module disable mysql -y 
+dnf module disable mysql -y &>>/tmp/expense.log
 
 echo -e "\e[31m Creating repo file \e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/expense.log
 
 echo -e "\e[31m Installing mysql-community-server\e[0m"
-dnf install mysql-community-server -y
+dnf install mysql-community-server -y &>>/tmp/expense.log
 
 echo -e "\e[31m Starting mysql-community-server\e[0m"
-systemctl enable mysql
-systemctl start mysql
+systemctl enable mysql &>>/tmp/expense.log
+systemctl start mysql &>>/tmp/expense.log
 
 echo -e "\e[31m Setting password to mysql-community-server\e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>/tmp/expense.log
